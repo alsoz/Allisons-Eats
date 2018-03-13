6c3d89bfdde483a3f25ca438a96a738e8e8302d5
Unknown <jef11@aber.ac.uk>
2018-03-13 22:21:11 +0000

    Added top button and moved Stew into directory
    Added top button to index, and the three main pages. Added .js files were needed, added bits to bottom of css files and edited the html to add the button and links the .js file. 
    
    Also added stew files into a directory for consistency.

--- Healthy MacnCheese/HealthyMacnCheese.js
+++ Healthy MacnCheese/HealthyMacnCheese.js
@@ -4,3 +4,19 @@ $('.dropdown-toggle').on('show.bs.dropdown',function () {
 $('.navbar-toggle').on('show.bs.collapse', function () {
 
 });
+
+//from W3Schools
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-$('.dropdown-toggle').on('show.bs.dropdown',function () {
 
-});
-$('.navbar-toggle').on('show.bs.collapse', function () {
+// Some old code
+//$('.dropdown-toggle').on('show.bs.dropdown',function () {
+//
+//});
+//$('.navbar-toggle').on('show.bs.collapse', function () {
+//
+//});
 
-});
+//from W3Schools
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
